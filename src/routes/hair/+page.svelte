4f3411681b81a758a4b0081a7059c342699523c9
kian ariai <R.lotfalizadeh57@yahoo.com>
2026-09-17 01:55:30 +0330

    feat: add hair visual and Zetamed science link

--- src/routes/hair/+page.svelte
+++ src/routes/hair/+page.svelte
@@ -38,4 +38,9 @@
   ctaLabel="درخواست ارزیابی مو"
   backHref="/"
   backLabel="بازگشت به صفحه اصلی"
+  visualKind="hair"
+  visualLabel="HAIR • FOLLICLE"
+  scienceHref="https://zetamed.org/%D9%81%D8%A7%DA%A9%D8%AA%D9%88%D8%B1%D9%87%D8%A7%DB%8C-%D8%B1%D8%B4%D8%AF%DB%8C-%DA%A9%D9%87-%D8%A7%D8%B2-%D9%BE%D9%84%D8%A7%DA%A9%D8%AA%D9%87%D8%A7-%D8%A8%D9%87-%D8%AF%D8%B3%D8%AA-%D9%85/"
+  scienceTitle="فاکتورهای رشد پلاکتی و بافت مو"
+  scienceText="اگر می‌خواهید درباره فاکتورهای رشد پلاکتی و نقش آن‌ها در پیام‌رسانی و ترمیم بافت بیشتر بدانید، مطلب علمی Zetamed را مطالعه کنید."
 />
