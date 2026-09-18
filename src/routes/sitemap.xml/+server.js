9e4b0fca640519474150a4e6027f7a7c11169947
kian ariai <R.lotfalizadeh57@yahoo.com>
2026-09-18 08:07:24 +0330

    seo: add article URLs to sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,6 +1,12 @@
 import { site } from '$lib/data/site.js';
+import { articles } from '$lib/data/articles.js';
+
 export const prerender = true;
-const paths = ['', '/orthopedics','/orthopedics/knee','/orthopedics/shoulder','/hair','/skin','/wounds','/sexual-health','/bio-harmony','/technologies','/articles','/doctors','/about','/contact'];
+
+const staticPaths = ['', '/orthopedics','/orthopedics/knee','/orthopedics/shoulder','/hair','/skin','/wounds','/sexual-health','/bio-harmony','/technologies','/articles','/doctors','/about','/contact'];
+const articlePaths = articles.map((article) => `/articles/${article.slug}`);
+const paths = [...staticPaths, ...articlePaths];
+
 export function GET() {
   const urls = paths.map((p) => `<url><loc>${site.domain}${p}</loc></url>`).join('');
   return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`, { headers: { 'content-type': 'application/xml' } });
