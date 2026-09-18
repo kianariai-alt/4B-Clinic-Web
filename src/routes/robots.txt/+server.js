63a0aba647b747e3fea7745fd717164a26dc53a2
kian ariai <R.lotfalizadeh57@yahoo.com>
2026-09-18 15:15:48 +0330

    seo: keep admin out of crawler paths

--- src/routes/robots.txt/+server.js
+++ src/routes/robots.txt/+server.js
@@ -1,5 +1,8 @@
 import { site } from '$lib/data/site.js';
 export const prerender = true;
 export function GET() {
-  return new Response(`User-agent: *\nAllow: /\nSitemap: ${site.domain}/sitemap.xml\n`, { headers: { 'content-type': 'text/plain' } });
+  return new Response(
+    `User-agent: *\nAllow: /\nDisallow: /admin\nSitemap: ${site.domain}/sitemap.xml\n`,
+    { headers: { 'content-type': 'text/plain; charset=utf-8' } }
+  );
 }
