ead2ca05d63cecaa277fcb3861cdfed13a492cc0
kian ariai <R.lotfalizadeh57@yahoo.com>
2026-09-18 15:15:45 +0330

    seo: add article lastmod to sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -4,10 +4,20 @@ import { publishedArticles as articles } from '$lib/data/articles.js';
 export const prerender = true;
 
 const staticPaths = ['', '/orthopedics','/orthopedics/knee','/orthopedics/shoulder','/hair','/skin','/wounds','/sexual-health','/bio-harmony','/technologies','/articles','/doctors','/about','/contact'];
-const articlePaths = articles.map((article) => `/articles/${article.slug}`);
-const paths = [...staticPaths, ...articlePaths];
+
+function urlEntry(path, lastmod = '') {
+  const modified = lastmod ? `<lastmod>${lastmod.slice(0, 10)}</lastmod>` : '';
+  return `<url><loc>${site.domain}${path}</loc>${modified}</url>`;
+}
 
 export function GET() {
-  const urls = paths.map((p) => `<url><loc>${site.domain}${p}</loc></url>`).join('');
-  return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`, { headers: { 'content-type': 'application/xml' } });
+  const staticUrls = staticPaths.map((path) => urlEntry(path));
+  const articleUrls = articles.map((article) =>
+    urlEntry(`/articles/${article.slug}`, article.updatedAt || article.createdAt || '')
+  );
+  const urls = [...staticUrls, ...articleUrls].join('');
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`,
+    { headers: { 'content-type': 'application/xml; charset=utf-8' } }
+  );
 }
