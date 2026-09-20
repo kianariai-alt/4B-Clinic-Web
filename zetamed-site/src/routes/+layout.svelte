bf48730e7811c875294af82993f60062a1db6124
kian ariai <R.lotfalizadeh57@yahoo.com>
2026-09-20 09:59:45 +0330

    chore(zetamed): add src/routes/+layout.svelte

---
+++ zetamed-site/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Header from '$lib/components/Header.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+  import '../app.css';
+  let { children } = $props();
+</script>
+<Header />
+<main>{@render children()}</main>
+<Footer />
