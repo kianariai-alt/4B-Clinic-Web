097cf36a062e0c45591cb06c959f62b358c56d17
kian ariai <R.lotfalizadeh57@yahoo.com>
2026-09-20 09:59:34 +0330

    chore(zetamed): add svelte.config.js

---
+++ zetamed-site/svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({ fallback: '404.html' }),
+    prerender: { entries: ['*'] },
+    trailingSlash: 'always'
+  }
+};
+
+export default config;
