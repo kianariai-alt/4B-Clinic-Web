9861878acba3b547613961c9eb119e77dffcd66b
kian ariai <R.lotfalizadeh57@yahoo.com>
2026-09-25 00:29:48 +0330

    chore(seo): add hair intent routes to sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -3,7 +3,7 @@ import { publishedArticles as articles } from '$lib/data/articles.js';
 
 export const prerender = true;
 
-const staticPaths = ['', '/orthopedics','/orthopedics/knee','/orthopedics/knee/osteoarthritis-non-surgical','/orthopedics/knee/meniscus-non-surgical','/orthopedics/knee/knee-wear','/orthopedics/shoulder','/hair','/skin','/wounds','/sexual-health','/bio-harmony','/technologies','/articles','/doctors','/about','/contact','/editorial-policy'];
+const staticPaths = ['', '/orthopedics','/orthopedics/knee','/orthopedics/knee/osteoarthritis-non-surgical','/orthopedics/knee/meniscus-non-surgical','/orthopedics/knee/knee-wear','/orthopedics/shoulder','/hair','/hair/hair-loss-control','/hair/non-surgical-hair-loss','/hair/pl-prgf','/skin','/wounds','/sexual-health','/bio-harmony','/technologies','/articles','/doctors','/about','/contact','/editorial-policy'];
 
 /** @param {string} path @param {string} [lastmod] */
 function urlEntry(path, lastmod = '') {
