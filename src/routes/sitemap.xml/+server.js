2cd23a616a6eabe9b3d953bced9a8f5b7855785d
kian ariai <R.lotfalizadeh57@yahoo.com>
2026-09-25 00:45:59 +0330

    chore(seo): add sexual health intent routes to sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -3,7 +3,7 @@ import { publishedArticles as articles } from '$lib/data/articles.js';
 
 export const prerender = true;
 
-const staticPaths = ['', '/orthopedics','/orthopedics/knee','/orthopedics/knee/osteoarthritis-non-surgical','/orthopedics/knee/meniscus-non-surgical','/orthopedics/knee/knee-wear','/orthopedics/shoulder','/hair','/hair/hair-loss-control','/hair/non-surgical-hair-loss','/hair/pl-prgf','/skin','/skin/non-surgical-rejuvenation','/skin/needle-free-rejuvenation','/skin/pl-prgf-rejuvenation','/skin/scar/non-surgical-scar-treatment','/skin/scar/hypertrophic-keloid','/skin/scar/acne-scars','/wounds','/wounds/chronic-wound-treatment','/wounds/diabetic-foot','/wounds/diabetic-wound-red-flags','/sexual-health','/bio-harmony','/technologies','/articles','/doctors','/about','/contact','/editorial-policy'];
+const staticPaths = ['', '/orthopedics','/orthopedics/knee','/orthopedics/knee/osteoarthritis-non-surgical','/orthopedics/knee/meniscus-non-surgical','/orthopedics/knee/knee-wear','/orthopedics/shoulder','/hair','/hair/hair-loss-control','/hair/non-surgical-hair-loss','/hair/pl-prgf','/skin','/skin/non-surgical-rejuvenation','/skin/needle-free-rejuvenation','/skin/pl-prgf-rejuvenation','/skin/scar/non-surgical-scar-treatment','/skin/scar/hypertrophic-keloid','/skin/scar/acne-scars','/wounds','/wounds/chronic-wound-treatment','/wounds/diabetic-foot','/wounds/diabetic-wound-red-flags','/sexual-health','/sexual-health/erectile-dysfunction-evaluation','/sexual-health/female-dryness-pain','/sexual-health/o-shot-p-shot-guide','/bio-harmony','/technologies','/articles','/doctors','/about','/contact','/editorial-policy'];
 
 /** @param {string} path @param {string} [lastmod] */
 function urlEntry(path, lastmod = '') {
